a06b9928e508fe774088947b57f271631c83c2e3
PLRG <plrg@PLRGs-MacBook-Pro.local>
2021-08-11 23:24:13 +0900

    save

--- 2468_dfs_recur.py
+++ 2468_dfs_recur.py
@@ -1,7 +1,9 @@
 import sys 
 sys.setrecursionlimit(10000) 
+
 dx = [1, -1, 0, 0] 
 dy = [0, 0, 1, -1] 
+
 def dfs(x, y, h): 
     for i in range(4): 
         nx = x + dx[i] 
@@ -10,13 +12,22 @@ def dfs(x, y, h):
             if arr[nx][ny] > h and done[nx][ny] == 0: 
                 done[nx][ny] = 1 
                 dfs(nx, ny, h) 
-N = int(input()) 
-arr = [list(map(int, input().split())) for _ in range(N)] 
+
+N = int(sys.stdin.readline())
+arr = []
+
+for insert_arr in range(N):
+    temp_arr = []
+    for temp in map(int, sys.stdin.readline().split()):
+        temp_arr.append(temp)
+    arr.append(temp_arr)
+
 ans = 0 
 
+### increase water ###
 for k in range(max(map(max, arr))): 
     cnt = 0 
-    done = [[0]*N for _ in range(N)] 
+    done = [[0]*N for _ in range(N)]
     for i in range(N): 
         for j in range(N): 
             if arr[i][j] > k and done[i][j] == 0: 
