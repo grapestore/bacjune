968084b4caf126318108f70c17879ebf85c37d1b
PLRG <plrg@PLRGs-MacBook-Pro.local>
2021-08-13 13:47:19 +0900

    save

--- 2470.py
+++ 2470.py
@@ -1,34 +1,26 @@
-# import sys
+import sys
 
-# def permutations(ref, n):
-
-#     ans = []
-#     if n > len(ref): return ans
-#     if n == 1:
-#         for item in ref: ans.append([item])
-#     elif n>1:
-#         for i in range(len(ref)):
-#             temp_ref = ref[:]
-#             temp_ref.remove(ref[i]) 
-#             for p in permutations(temp_ref, n-1):
-#                 ans.append([ref[i]]+p)
-#     return ans
-
-# num = int(input())
-# arr = []
-# result = [0,0]
-# min_max = 100000
-# for x in map(int,sys.stdin.readline().split()):
-#     arr.append(x)
-# set(arr)
-# for arr_check in permutations(arr,2):
-#     #print(arr_check, abs(arr_check[0] + arr_check[1]), min_max)
-#     if abs(arr_check[0] + arr_check[1]) < min_max:
-#         #print(arr_check, abs(arr_check[0] + arr_check[1]), min_max)
-#         min_max = abs(arr_check[0] + arr_check[1])
-#         result[0] = arr_check[0]
-#         result[1] = arr_check[1]
-# result.sort()
-# print(result)
-
+num = int(input())
 arr = []
+result = [0,0]
+min_max = 2000000000
+for x in map(int,sys.stdin.readline().split()):
+    arr.append(x)
+arr.sort()
+left = 0
+right = len(arr)-1
+target = 0
+while right - left >0:
+    if abs(arr[right] + arr[left]) == target:
+        result[0] = arr[left]
+        result[1] = arr[right]
+        break
+    if abs(arr[left] + arr[right]) < min_max:
+        min_max = abs(arr[left] + arr[right])
+        result[0] = arr[left]
+        result[1] = arr[right]
+    if arr[left] + arr[right]>0:
+        right -= 1
+    elif arr[left] + arr[right]<0:
+        left += 1
+print(result)
