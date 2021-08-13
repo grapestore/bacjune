bd26490a118c499bcd3b4e40f6e0db97dea47db0
inkyu <inkyu@inkyuui-MacBookAir.local>
2021-08-13 23:42:10 +0900

    save

--- 6549.py
+++ 6549.py
@@ -1,2 +1,32 @@
-print('hello')
+import sys
 
+def solves(left,right):
+    if left==right:
+        return arr[left]
+
+    mid = (left+right)//2
+    left_location = mid
+    right_location = mid+1
+    current_height = min(arr[left_location],arr[right_location])
+    max_area = current_height * (right_location-left_location+1)
+
+    while left_location>left or right_location<right:
+        if right_location<right and (arr[left_location-1] < arr[right_location+1] or left_location==left):
+            right_location += 1
+            current_height = min(current_height, arr[right_location])
+        else:
+            left_location -= 1
+            current_height = min(current_height, arr[left_location])
+        max_area = max(max_area ,current_height * (right_location-left_location+1))
+    
+    return max(solves(left,mid),solves(mid+1,right),max_area)
+
+
+
+while True:
+    arr = list(map(int,sys.stdin.readline().split()))
+    right = arr[0]
+    if right == 0:
+        break
+    print(solves(1,right))
+    
