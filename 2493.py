6877cfe2061bd1c9f06b737a3b9f85b8826863a6
inkyu <inkyu@inkyuui-MacBookAir.local>
2021-08-14 14:27:10 +0900

    save

--- 2493.py
+++ 2493.py
@@ -1,14 +1,20 @@
-stk = [6, 9, 5, 7, 4]
-result = []
-for i in reversed(stk):
-    for j in reversed(range(len(stk)-1)):
-        if i < stk[j]:
-            result.append(j+1)
-            stk.pop()
-            break
-        elif i>stk[j] and j ==0:
-            result.append(0)
-result.append(0)
-print(result)
-for k in reversed(result):
-    print(k, end=' ')
+import sys
+
+# num = int(input())
+# stk = list(map(int,sys.stdin.readline().split()))
+stk = [6,9,5,7,4]
+result = [0]
+prev = stk[0]
+maximum = stk[0]
+for x in stk[1:]:
+    if prev < x:
+        if maximum < x:
+            
+        
+        else:
+
+    else:
+
+# for k in reversed(result):
+#     j = str(j) + ' ' + str(k)
+# print(j)
