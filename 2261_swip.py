a6d3ef1bc06f93b1012bf4f137145bbc87e7f1e8
inkyu <inkyu@inkyuui-MacBookAir.local>
2021-08-18 20:22:45 +0900

    save

--- 2261_swip.py
+++ 2261_swip.py
@@ -1,33 +1,38 @@
 import sys, heapq, math
+from collections import deque
+
+def dis_check(start,end):
+    if end-start < 1:
+        return sys.maxsize
+    mid = (start+end)//2
+    data_mid = math.sqrt((heap[mid+1][1] - heap[mid][1])**2 + (heap[mid+1][0] - heap[mid][0])**2)
+    return min(dis_check(start,mid), data_mid, dis_check(mid+1,end))
+
 
 num = int(sys.stdin.readline())
 spot = []
 for x in range(num):
     temp = list(map(int,sys.stdin.readline().split()))
     spot.append(temp)
-heap = []
+heap = deque()
 spot.sort(key=lambda x: (x[0]))
 distance = math.sqrt((spot[1][0] - spot[0][0])**2 + (spot[1][1] - spot[0][1])**2)
 new_distance = sys.maxsize
 min_data = (spot[1][0] - spot[0][0])**2 + (spot[1][1] - spot[0][1])**2
 width_x = [0,0]
 width_y = [0,0]
-for spot_lo in spot:
-    width_x[1] = spot_lo[0]
+for x in range(len(spot)):
+    width_x[1] = spot[x][0]
     width_x[0] = width_x[1] - distance
-    width_y[1] = spot_lo[1] + distance
-    width_y[0] = spot_lo[1] - distance
-    heapq.heappush(heap, spot_lo)
+    width_y[1] = spot[x][1] + distance
+    width_y[0] = spot[x][1] - distance
+    heap.append(spot[x])
     while len(heap)>0 and (heap[0][0] < width_x[0]):
-        heapq.heappop(heap)
+        heap.popleft()
     if len(heap)>1:
-        for i in range(0,len(heap)-1):
-            for j in range(i+1,len(heap)):
-                new_distance = math.sqrt((heap[i][0] - heap[j][0])**2 + (heap[i][1] - heap[j][1])**2)
-                if distance > new_distance:
-                    min_data = (heap[i][0] - heap[j][0])**2 + (heap[i][1] - heap[j][1])**2
-                    distance = new_distance
-                    break
-print(min_data)
+        new_distance = dis_check(0,len(heap)-1)
+        if distance > new_distance:
+            distance = new_distance
+print(distance**2)
 
 
