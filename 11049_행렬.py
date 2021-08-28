e5b2df170312121300d2e05c8a9d2bf84f73508b
inkyu <inkyu@inkyuui-MacBookAir.local>
2021-08-28 10:01:04 +0900

    save

--- 11049_행렬.py
+++ 11049_행렬.py
@@ -2,12 +2,18 @@ import sys
 sys.stdin = open('inputs.text')
 
 n = int(sys.stdin.readline())
-memo = dict()
+memo = [[0]*n for _ in range(n)]
+
+
 arr = []
 for _ in range(n):
     arr.append(list(map(int,sys.stdin.readline().split())))
-a = arr[0]
-b = arr[1]
-c = arr[2]
-memo[3] = min(a[0]*b[0]*b[1] + a[0]*b[1]*c[1], 
-print(memo)
+
+for i in range(1, n): 
+    for j in range(n-i): 
+        memo[j][j+i] = float('inf')
+        for k in range(j, j+i): 
+            memo[j][j+i] = min(memo[j][j+i], memo[j][k] + memo[k+1][j+i] + arr[j][0]*arr[k][1]*arr[j+i][1])
+
+
+print(memo[0][n-1])
