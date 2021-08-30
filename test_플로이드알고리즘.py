dc87de6b423ade218e81a301228ad0a90592b575
inkyu <inkyu@inkyuui-MacBookAir.local>
2021-08-30 18:38:06 +0900

    save

--- test_플로이드알고리즘.py
+++ test_플로이드알고리즘.py
@@ -1,1 +1,27 @@
-print('hello')
+inf = 999
+W = [
+    [0,1,inf,1,5],
+    [9,0,3,2,inf],
+    [inf,inf,0,4,inf],
+    [inf,inf,2,0,3],
+    [3,inf,inf,inf,0]]
+
+
+
+
+def floyd(w):
+    D = w
+    n = len(w)
+    P = [[-1]*n for _ in range(n)]
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if D[i][j] > D[i][k]+D[k][j]:
+                    D[i][j] = D[i][k]+D[k][j]
+                    P[i][j] = k
+
+    return D, P
+
+D,P = floyd(W)
+for x in P:
+    print(x)
