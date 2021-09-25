4b131754b3fa45227de69cd5b7b306c1580ba23f
inkyu <inkyu@inkyuui-MacBookAir.local>
2021-09-25 21:44:22 +0900

    save

---
+++ 1406_에디터.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open('inputs.text')
+
+lstack = list(sys.stdin.readline().strip())
+rstack = []
+
+N = int(sys.stdin.readline())
+
+for _ in range(N):
+    cmd = sys.stdin.readline()
+    if cmd[0] == 'L': 
+        if lstack:
+            rstack.append(lstack.pop())
+    elif cmd[0] == 'D': 
+        if rstack:
+            lstack.append(rstack.pop())
+    elif cmd[0] == 'B': 
+        if lstack:
+            lstack.pop()
+    else:
+        lstack.append(cmd[2])
+
+print(''.join(lstack + rstack[::-1]))
