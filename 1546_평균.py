2d247269156d779d3cdec9dc18588d18f4b0ba6e
inkyu <inkyu@inkyuui-MacBookAir.local>
2021-10-04 23:47:22 +0900

    sasve

---
+++ 1546_평균.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('inputs.text')
+
+n = int(sys.stdin.readline())
+arr = list(map(int,sys.stdin.readline().split()))
+m = max(arr)
+
+for i in range(n):
+    arr[i] = arr[i]/m*100
+
+print(sum(arr)/n)
