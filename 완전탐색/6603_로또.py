6c49df2d6ca21afd575614271ef0ac26cd038087
inkyu <inkyu@inkyuui-MacBookAir.local>
2021-12-16 14:49:02 +0900

    save

--- 완전탐색/6603_로또.py
+++ 완전탐색/6603_로또.py
@@ -1,4 +1,24 @@
 import sys
 sys.stdin = open('inputs.text')
-from itertools import permutations
+from itertools import combinations
+
+
+def gen_combinations(arr, n):
+  result =[]
+  if n == 0:
+    return [[]]
+  for i,elem in enumerate(arr):
+    for C in gen_combinations(arr[i + 1:], n-1):
+      result += [[elem]+C]
+  return result
+
+while True:
+  arr = list(map(int,sys.stdin.readline().split()))
+  status = arr.pop(0)
+  if status == 0:
+    break
+  for data in gen_combinations(arr,6):
+    print(*data)
+  print()
+
 
