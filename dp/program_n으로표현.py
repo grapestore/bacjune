2a21c0d6520b0b5d2052dc6873b84041c52993a2
inkyu <inkyu@inkyuui-MacBookAir.local>
2021-12-31 19:00:26 +0900

    save

--- dp/program_n으로표현.py
+++ dp/program_n으로표현.py
@@ -2,8 +2,9 @@ def dfs(count, num,target):
     global result
     if count > 8:
         return
-    if num == target and count < result:
-        result = count
+    if num == target:
+        if count < result:
+            result = count
     for i in range(8):
         dfs(count+i+1, num+NNN[i], target)
         dfs(count+i+1, num-NNN[i], target)
@@ -28,3 +29,47 @@ def solution(N, number):
         return -1
     else:
         return result
+
+
+
+
+# from collections import deque
+
+# def bfs(target):
+#     global result
+#     queue = deque([[0,0]])
+    
+#     while queue:
+#         count, num = queue.popleft()
+#         if count>8:
+#             continue
+#         if count>result:
+#             break
+#         if num == target:
+            
+#             result = count
+        
+#         for i in range(8):
+#             queue.append([count+i+1, num+NNN[i]])
+#             queue.append([count+i+1, num-NNN[i]])
+#             queue.append([count+i+1, num*NNN[i]])
+#             queue.append([count+i+1, num//NNN[i]])
+        
+#     return
+
+# def solution(N, number):
+#     global NNN, result
+#     strN = str(N)
+#     NNN = []
+#     result = 9
+#     temp = strN
+#     for _ in range(8):
+#         NNN.append(int(temp))
+#         temp+=strN
+
+#     bfs(number)
+#     print(result)
+#     if result>8:
+#         return -1
+#     else:
+#         return result
