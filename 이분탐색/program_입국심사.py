b417c901ceb7aecd86b7be9836b93ceaae8dbd10
inkyu <inkyu@inkyuui-MacBookAir.local>
2022-01-02 20:24:32 +0900

    save

---
+++ 이분탐색/program_입국심사.py
@@ -0,0 +1,20 @@
+def solution(n, times):
+    
+    left = 1
+    right = max(times)*n
+    answer = 1e18
+    while left<=right:
+        result = [0] * len(times)
+        mid = (left+right)//2
+        for i in range(len(times)):
+            result[i] = mid//times[i]
+
+        result_human = sum(result)
+        if result_human<n:
+            left = mid+1
+        else:
+            if mid<=answer:
+                answer = mid
+            right = mid-1
+        
+    return answer
