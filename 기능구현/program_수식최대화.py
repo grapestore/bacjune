58364d90edb40354792a3cd5e3705f70c75bae3f
grapestore <ikbae0131@gmail.com>
2022-01-09 18:41:37 +0900

    프로그래머스 수식최대화

---
+++ 기능구현/program_수식최대화.py
@@ -0,0 +1,16 @@
+import re
+from itertools import permutations
+expression = "100-200*300-500+20"
+    
+
+
+operators = ["*", "+", "-"]
+answer = []
+for oper in permutations(operators, 3):
+    a = oper[0]
+    b = oper[1]
+    tmp_list = []
+    for i in expression.split(a):
+        tmp = [f"({j})" for j in i.split(b)]
+        tmp_list.append(f"({b.join(tmp)})")
+    answer.append(abs(eval(a.join(tmp_list))))
